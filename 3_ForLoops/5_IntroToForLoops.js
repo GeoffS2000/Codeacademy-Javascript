b17cd9e8fcad635646831e7b249c0be330ab2537
GeoffS2000 <user2000user2000@server.fake>
2013-11-15 16:30:07 -0800

    finished the for loops part

--- 3_ForLoops/5_IntroToForLoops.js
+++ 3_ForLoops/5_IntroToForLoops.js
@@ -1,4 +1,22 @@
 //Intro to for loops
 
+// Example of a for loop:
+
+for (var counter = 1; counter < 6; counter++) {
+	console.log(counter);
+}
+
+
+// Edit this for loop!
+
+for (var i = 5; i < 51; i +=5) {
+	console.log(i);
+}
+
+
+// Write your very own for loop!
+for (var i = 100; i > 0; i -=5) {
+    console.log(i);
+}
 
 
